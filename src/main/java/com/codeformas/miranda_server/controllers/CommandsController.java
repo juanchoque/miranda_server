deeeab9948fb4cb560afebbb25a75168e35ffa48
juan1206 <juanchoque1206@gmail.com>
2018-09-07 08:59:10 -0400

    finish data

--- src/main/java/com/codeformas/miranda_server/controllers/CommandsController.java
+++ src/main/java/com/codeformas/miranda_server/controllers/CommandsController.java
@@ -2,6 +2,7 @@ package com.codeformas.miranda_server.controllers;
 
 import com.codeformas.miranda_server.model.domain.Command;
 import com.codeformas.miranda_server.sockets.services.SocketClientService;
+import com.codeformas.miranda_server.util.ConstantMiranda;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 public class CommandsController {
 
     @ResponseBody
-    @RequestMapping(value = "/sendcommand", method = RequestMethod.POST, produces = "application/json")
+    @RequestMapping(value = "/sendcommand", method = RequestMethod.POST, produces = ConstantMiranda.APP_JSON)
     public ResponseEntity<Object> listUbications(@RequestBody Command command) {
         String respuestaJson = "";
         HttpStatus httpStatus = HttpStatus.OK;
